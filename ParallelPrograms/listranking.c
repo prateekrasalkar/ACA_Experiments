4786b34252010311f1c2291126c0c26c94554f52
Prateek Rasalkar <prasalkar916@gmail.com>
2025-04-27 23:51:49 +0530

    Create listranking.c

---
+++ ParallelPrograms/listranking.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<omp.h>
+int main() {
+int arr[] = {5, 2, 9, 1, 5, 6};
+int n=6;
+int i, j, temp;
+#pragma omp parallel for
+for (i = n ;i > 0;i--) {
+#pragma omp parallel for
+for (j = i-1; j > 0; j--) {
+if (arr[i] < arr[j ]) {
+temp = arr[i];
+arr[i] = arr[j];
+arr[j ] = temp;
+}
+}
+}
+int p=1;
+for (int i = n-1; i >=0; i--) {
+printf("Element = %d Rank = %d \n", arr[i],p);
+p++;
+}
+printf("\n");
+return 0;
+}
