19bda741cd93f2f7a7c6b26ac485c2daa712bf6e
Prateek Rasalkar <prasalkar916@gmail.com>
2025-04-27 16:43:58 +0530

    Create BinarySearch.c

---
+++ Searching/BinarySearch.c
@@ -0,0 +1,8 @@
+Time Complexity :- 
+Best Case: O(1)
+Average Case: O(log N)
+Worst Case: O(log N)
+
+Auxiliary Space: O(1), If the recursive call stack is considered then the auxiliary space
+will be O(logN).
+
