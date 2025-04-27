fc09875468bec92fddc85b91b116f9fa2caa803e
Prateek Rasalkar <prasalkar916@gmail.com>
2025-04-27 23:21:45 +0530

    Create prefixsum.c

---
+++ ParallelPrograms/prefixsum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <omp.h>
+int main() {
+int N = 1000;
+int arr[N], result[N];
+double start, end;
+#pragma omp parallel for
+for (int i = 0; i < N; i++) {
+arr[i] = i + 1;
+}
+start = omp_get_wtime();
+result[0] = arr[0];
+for (int i = 1; i < N; i++) {
+result[i] = result[i - 1] + arr[i];
+}
+end = omp_get_wtime();
+printf("Final Prefix Sum: %d\n", result[N - 1]);
+printf("Prefix Sum Computation Completed in %f seconds.\n", end - start);
+return 0;
+}
