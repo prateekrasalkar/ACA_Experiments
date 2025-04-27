8ef4c761bd05429b166680b589ded5e334b1292d
Prateek Rasalkar <prasalkar916@gmail.com>
2025-04-27 16:44:17 +0530

    Update Linearsearch.c

--- Searching/Linearsearch.c
+++ Searching/Linearsearch.c
@@ -1,3 +1,13 @@
+ Time Complexity:
+ Best Case: In the best case, the key might be present at the first index. So the
+best case complexity is O(1)
+ Worst Case: In the worst case, the key might be present at the last index i.e.,
+opposite to the end from which the search has started in the list. So the worstcase
+complexity is O(N) where N is the size of the list.
+ Average Case: O(N)
+ Auxiliary Space: O(1) as except for the variable to iterate through the list, no other
+variable is used.
+
 #include<iostream>
 using namespace std;
 int main(){
